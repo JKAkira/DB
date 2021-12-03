5386be694359f209c9cce69e4a5220ced3df6110
JKAkira <kchonlatharn.tccmtc@gmail.com>
2021-12-03 13:09:04 +0700

    first commit

---
+++ server/app.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const path = require('path');
+const body = require('body-parser');
+//const app = express();
+const mysql = require('mysql');
+
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('server.key', 'utf8');
+var certificate = fs.readFileSync('server.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+const app = express();
+
+var httpsServer = https.createServer(credentials, app);
+
+app.use(body());
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+const db = mysql.createConnection({
+    host: '192.168.1.7',
+    user: 'ppap',
+    password: '1234',
+    database: 'expoint'
+});
+// show data
+app.get('/data', function(req,res){
+    console.log("Hello in /data ");
+    let sql = 'SELECT * FROM test123;';
+    db.query(sql, (err, result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.json(result);
+    });
+    console.log("after query");
+});
+
+//delete
+app.put('/delete', function(req, res) {
+    var sql = 'DELETE FROM test123 WHERE id_P = ?';
+    db.query(sql,[req.body.idkey],function (error, results) {
+        if(error) throw error;
+        res.send(JSON.stringify(results));
+    });
+});
+
+//edit
+app.put('/data', function(req, res) {
+    var sql = 'UPDATE test123 SET firstname_P= ? , lastname_P = ? WHERE id_P = ?';
+    db.query(sql,[req.body.firstname,req.body.lastname,req.body.idkey],function (error, results) {
+        if(error) throw error;
+        res.send(JSON.stringify(results));
+    });
+});
+
+//insert
+app.post('/data', function(req, res){
+    console.log(req.body);
+    let data = {
+        id_P:req.body.idkey,
+        firstname_P:req.body.firstname,
+        lastname_P:req.body.lastname
+    };
+    let sql = 'INSERT INTO test123 SET ?';
+    db.query(sql, data, (err, result)=>{
+        if(err){
+            console.log(err);
+            console.log("ID is Primarykey!!!!!");
+            console.log("Enter the id again..");
+        }else{
+            console.log(result);
+        }
+    });
+});
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+
+
+
+//module.exports = app;
+module.exports = httpsServer;
